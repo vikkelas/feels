38629206414c247554e0f064cb93f48d6388f06e
Vikkelas <bass89@mail.ru>
2022-02-11 15:35:31 +0300

    version 1.0 tutorial

---
+++ README.md
@@ -0,0 +1,3 @@
+# check card
+https://vikkelas.github.io/ahj-homework-4/
+[![Build status](https://ci.appveyor.com/api/projects/status/fjkf9gcelkvthe4s/branch/master?svg=true)](https://ci.appveyor.com/project/Pavel-A-T/goblingame/branch/master)
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  devServer: {
+    port: 9000,
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+        }],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: false,
+          },
+        }],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
